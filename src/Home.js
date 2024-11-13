86b663387dd56359add66245b296e7c9e1ccf073
AdarshFiveonline <helpdesk@fiveonline.in>
2024-11-13 18:36:57 +0530

    added new sections

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "./components/Layout";
 import SlickSlider from "./components/SlickSlider";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   const BannerSlider = [
@@ -22,6 +23,28 @@ const Home = () => {
     {
       image: "/images/home-banners/banner-4.jpg",
       text: "Speed, safety, and seemless operations combined",
+    },
+  ];
+
+  const ProductsSlider = [
+    {
+      image: "/images/products/product1.jpg",
+      productName: "Prime 2.0",
+    },
+
+    {
+      image: "/images/products/product1.jpg",
+      productName: "Prime 2.0",
+    },
+
+    {
+      image: "/images/products/product1.jpg",
+      productName: "Prime 2.0",
+    },
+
+    {
+      image: "/images/products/product1.jpg",
+      productName: "Prime 2.0",
     },
   ];
 
@@ -54,6 +77,34 @@ const Home = () => {
     autoplay: false,
   };
 
+  const ProductsSettings = {
+    centerMode: true,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    centerPadding: "50px",
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          centerPadding: "20px",
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "6px",
+        },
+      },
+    ],
+  };
+
   return (
     <Layout>
       <section className="home-banner">
@@ -64,7 +115,72 @@ const Home = () => {
         />
       </section>
 
-      <section className="about-section"></section>
+      <section className="about-section position-relative">
+        <div className="container">
+          <div className="col-lg-12">
+            <div className="row about-row align-items-center justify-content-center">
+              <div className="col-lg-6">
+                <div className="about-img-div">
+                  <img
+                    src="/images/about1.jpg"
+                    className="about-img1"
+                    alt="about-img1"
+                  />
+
+                  <img
+                    src="/images/about1.jpg"
+                    className="about-img2"
+                    alt="about-img2"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-6 mt-lg-0 mt-5">
+                <div className="about-text">
+                  <h5 className="about-subtitle">
+                    <span></span> Since 1996
+                  </h5>
+
+                  <h2>Ashish Rolling Shutter</h2>
+                  <p>
+                    We take immense gratification in introducing ourselves as
+                    the pioneer manufacturers & Solution provider for all types
+                    of High End Automatic Entrance Doors & Loading Bay
+                    Equipments Our rich heritage and ethics comes from our
+                    parent company, A Mathurbhai & Co. started way back in 1982
+                    with a small manufacturing, development center & a handful
+                    of technicians & engineers. They gradually grew with strong
+                    expertise, hard work & loyal clientele. The product range
+                    was all types of conventional doors, rolling shutters &
+                    gates
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="products-section">
+        <NavLink to={"/"}>
+          <SlickSlider
+            items={ProductsSlider}
+            settings={ProductsSettings}
+            hasText={false}
+            showProductName={true}
+          />
+        </NavLink>
+      </section>
+
+      <section className="featured-products-section">
+        <div className="container">
+          <div className="col-lg-12">
+            <div className="row">
+              <div className="col-lg-8"></div>
+              <div className="col-lg-4"></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
--- src/components/SlickSlider.js
+++ src/components/SlickSlider.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Slider from "react-slick";
+import { NavLink } from "react-router-dom";
 
-const SlickSlider = ({ items, settings, hasText }) => {
+const SlickSlider = ({ items, settings, hasText, showProductName }) => {
   return (
     <Slider {...settings}>
       {items.map((item, index) => (
-        <div key={index}>
+        <div className="element" key={index}>
           <img src={item.image} alt={`Slider item ${index}`} />
           {hasText && item.text && (
             <div className="home-banner-text">
               <h1>{item.text}</h1>
-              <button className="know-more-btn">
-                Know more <i className="fas fa-arrow-right"></i>
-              </button>
+              <NavLink to={"/"}>
+                <button className="know-more-btn">
+                  Know more <i className="fas fa-arrow-right"></i>
+                </button>
+              </NavLink>
             </div>
+          )}
+          {showProductName && (
+            <>
+              <div className="overlay-bg">
+                <img src="/images/top-right.png" alt="arrow-up" />
+              </div>
+              <div className="product-name">
+                <h4>{item.productName}</h4>
+                <NavLink to={"/about"}>
+                  <button className="discover-more-btn">Discover more</button>
+                </NavLink>
+              </div>
+            </>
           )}
         </div>
       ))}
