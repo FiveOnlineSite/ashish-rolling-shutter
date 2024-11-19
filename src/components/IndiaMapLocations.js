adda7f2d0a2c7a8948c1609461891d90b8f08c60
AdarshFiveonline <helpdesk@fiveonline.in>
2024-11-19 11:02:54 +0530

    added changes in locations in map

--- src/components/IndiaMapLocations.js
+++ src/components/IndiaMapLocations.js
@@ -2,15 +2,16 @@ const IndiaMapLocations = [
   {
     name: "Ahmedabad",
     tel: "+9193423424",
-    position: { left: "19%", top: "45%" },
+    position: { left: "16%", top: "45%" },
   },
   {
     name: "Chandigarh",
     tel: "+9193423424",
-    position: { left: "31%", top: "21%" },
+    position: { left: "28%", top: "21%" },
   },
-  { name: "Mumbai", tel: "+9193423424", position: { left: "20%", top: "60%" } },
-  { name: "Delhi", tel: "+9193423424", position: { left: "32%", top: "29%" } },
+  { name: "Mumbai", tel: "+9193423424", position: { left: "17%", top: "60%" } },
+
+  { name: "Delhi", tel: "+9193423424", position: { left: "29%", top: "29%" } },
 ];
 
 export default IndiaMapLocations;
